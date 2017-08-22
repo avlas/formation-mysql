2fe21287f54665cee5a602f40e4c251a36589b87
Avl <alina.vlas@gmail.com>
2017-08-22 12:21:39 +0200

    Insertion donnees dans les tables

--- Restaurant.sql
+++ Restaurant.sql
@@ -1,8 +1,10 @@
 DROP DATABASE IF EXISTS formation;
 
+/* Creation d'une database */
 CREATE DATABASE IF NOT EXISTS formation;
 USE formation;
 
+/* Creation de la table - commande */
 CREATE TABLE IF NOT EXISTS commande(
 ID INTEGER NOT NULL AUTO_INCREMENT, 
 commandeName VARCHAR(25) NOT NULL, 
@@ -11,6 +13,7 @@ commandeTotal INTEGER,
 PRIMARY KEY(ID)
 ) ENGINE=InnoDB ;
 
+/* Creation de la table - plat */
 CREATE TABLE IF NOT EXISTS plat(
 ID INTEGER NOT NULL AUTO_INCREMENT, 
 platName VARCHAR(25) NOT NULL, 
@@ -18,12 +21,14 @@ platTarif INTEGER,
 PRIMARY KEY(ID)
 ) ENGINE=InnoDB ;
 
+/* Creation de la table - ingredient */
 CREATE TABLE IF NOT EXISTS ingredient(
 ID INTEGER NOT NULL AUTO_INCREMENT, 
 ingredientName VARCHAR(25),
 PRIMARY KEY(ID)
 ) ENGINE=InnoDB ;
 
+/* Creation de la table - commande_plats */
 CREATE TABLE IF NOT EXISTS commande_plats(
 ID INTEGER NOT NULL AUTO_INCREMENT, 
 commandeID INTEGER,
@@ -33,6 +38,7 @@ CONSTRAINT FK_cmdPlat_commandeID FOREIGN KEY(commandeID) REFERENCES formation.co
 CONSTRAINT FK_cmdPlat_platID FOREIGN KEY(platID) REFERENCES formation.plat(id) ON DELETE CASCADE
 ) ENGINE=InnoDB ;
 
+/* Creation de la table - plat_ingredients */
 CREATE TABLE IF NOT EXISTS plat_ingredients(
 platID INTEGER NOT NULL, 
 ingredientID INTEGER NOT NULL, 
@@ -42,6 +48,7 @@ CONSTRAINT FK_platIngr_platID FOREIGN KEY(platID) REFERENCES formation.plat(ID) 
 CONSTRAINT FK_platIngr_ingredientID FOREIGN KEY(ingredientID) REFERENCES formation.ingredient(ID) ON DELETE CASCADE
 ) ENGINE=InnoDB ;
 
+/* Creation de la table - depot */
 CREATE TABLE IF NOT EXISTS depot(
 ingredientID INTEGER NOT NULL AUTO_INCREMENT, 
 depotDate DATE,
@@ -49,3 +56,52 @@ quantite INTEGER,
 PRIMARY KEY (ingredientID, depotDate),
 CONSTRAINT FK_ingredientID FOREIGN KEY(ingredientID) REFERENCES formation.ingredient(ID) ON DELETE CASCADE
 ) ENGINE=InnoDB ;
+
+/* Insertion des donnees dans la table - commande */
+INSERT INTO commande (commandeName, commandeDate, commandeTotal) 
+VALUES 
+	('Cmd1', '2017-08-01', 10),
+	('Cmd2', '2017-08-01', 6),
+	('Cmd3', '2017-08-02', 16);
+
+/* Insertion des donnees dans la table -plat */
+INSERT INTO plat (platName, platTarif) 
+VALUES 
+	('Sandwish poulet', 5), 
+	('Sandwish saumon', 5), 
+	('Wraps poulet', 6), 
+    ('Wraps boeuf', 6);
+
+/* Insertion des donnees dans la table - ingredient */
+INSERT INTO ingredient (ingredientName) 
+VALUES 
+	('tortilla'), 
+	('pain'), 
+	('poulet'), 
+    ('saumon'),
+    ('boeuf');
+    
+/* Insertion des donnees dans la table - commande_plats */
+INSERT INTO commande_plats (commandeID, platID) 
+VALUES (1,1), (1,2), (2,3), (3,1), (3,2), (3,3);
+
+/* Insertion des donnees dans la table - plat_ingredients */
+INSERT INTO plat_ingredients (platID, ingredientID, quantite) 
+VALUES 
+	(1, 2, 200), 
+	(1, 3, 150), 
+	(2, 2, 200), 
+	(2, 4, 150), 
+	(3, 1, 100), 
+	(3, 2, 150), 
+	(4, 1, 100), 
+	(4, 5, 150);
+
+/* Insertion des donnees dans la table - depot */
+INSERT INTO depot (ingredientID, depotDate, quantite) 
+VALUES 
+	(1, '2017-08-01', 1000),
+	(2, '2017-08-02', 500),
+    (3, '2017-08-03', 2000),
+	(4, '2017-08-04', 1450),
+	(3, '2017-08-05', 700);
