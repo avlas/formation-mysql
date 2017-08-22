dd50a1167e2e96d1cc35428f0b0cad1e7dfc4c9d
Avl <alina.vlas@gmail.com>
2017-08-22 17:44:26 +0200

    partial commit

--- TP2/immobilier_questions.sql
+++ TP2/immobilier_questions.sql
@@ -1,54 +1,150 @@
 USE immobilier;
 
 /* Question 1 : Affichez le nom des agences */
-SELECT nom FROM agence;
+SELECT nom 
+FROM agence;
 
 /* Question 2 : Affichez le numéro de l’agence « Orpi » */
-SELECT idAgence FROM agence 
+SELECT idAgence 
+FROM agence 
 WHERE nom = 'Orpi';
 
 /* Question 3 : Affichez le premier enregistrement de la table logement */
-SELECT * FROM logement 
+SELECT * 
+FROM logement 
 WHERE idLogement = 5067;
+ -- or
+SELECT * 
+FROM logement 
+LIMIT 1;
 
 /* Question 4 : Affichez le nombre de logements (Alias : Nombre_de_logements) */
-SELECT count(*) AS Nombre_de_logements FROM logement;
+SELECT COUNT(*) AS Nombre_de_logements 
+FROM logement;
 
 /* Question 5 : Affichez les logements à vendre à moins de 150 000 € dans l’ordre croissant des prix. */
-SELECT * FROM logement 
+SELECT * 
+FROM logement 
 WHERE categorie = 'vente' AND prix < 150000 
 ORDER BY prix ASC;
 
 /* Question 6 : Affichez le nombre de logements à la location (alias : nombre) */
-SELECT count(*) AS nombre FROM logement 
+SELECT COUNT(*) AS nombre 
+FROM logement 
 WHERE categorie = 'location';
 
 /* Question 7 : Affichez les villes différentes recherchées par les personnes demandeuses d'un logement */
-SELECT DISTINCT ville FROM demande;
+SELECT DISTINCT ville 
+FROM demande;
 -- or
-SELECT DISTINCT ville FROM logement, logement_personne 
+SELECT DISTINCT ville 
+FROM logement, logement_personne 
 WHERE logement.idLogement = logement_personne.idLogement;
 
 /* Question 8 : Affichez le nombre de biens à vendre par ville */
-SELECT ville, count(*) AS nombre_de_logements FROM logement 
-WHERE categorie = 'vente' GROUP BY ville;
+SELECT ville, COUNT(*) AS nombre_de_logements 
+FROM logement 
+WHERE categorie = 'vente' 
+GROUP BY ville;
 
 /* Question 9 : Quelles sont les id des logements destinés à la location ? */
+SELECT idLogement 
+FROM logement 
+WHERE categorie = 'location';
+
 /* Question 10 : Quels sont les id des logements entre 20 et 30m² ? */
-/* Question 11 : Quel est le prix vendeur (hors commission) du logement le moins cher à vendre ? (Alias : prix minimum) */
+SELECT idLogement 
+FROM logement 
+WHERE superficie BETWEEN 20 AND 30;
+-- or
+SELECT idLogement 
+FROM logement 
+WHERE superficie  >= 20 AND superficie <= 30;
+
+/* Question 11 : Quel est le prix vendeur (hors commission) du logement le moins cher à vendre ? (Alias : prix_minimum) */
+SELECT min(prix) AS prix_minimum 
+FROM logement 
+WHERE categorie = 'vente';
+
 /* Question 12 : Dans quelle ville se trouve les maisons à vendre ? */
+SELECT ville 
+FROM logement 
+WHERE categorie = 'vente' AND genre='maison';
+
 /* Question 13 : L’agence Orpi souhaite diminuer les frais qu’elle applique sur le logement ayant l'id « 5246 ». Passer les frais de ce logement de 800 à 730€ */
+UPDATE logement_agence 
+SET frais=730 
+WHERE idLogement = 5246;
+-- Pour  verifier :
+SELECT frais 
+FROM logement_agence 
+WHERE idLogement = 5246;
+
 /* Question 14 : Quels sont les logements gérés par l’agence « laforet » */
+-- Version 1
+SELECT * 
+FROM logement 
+WHERE idLogement IN 
+	(SELECT la.idLogement FROM agence a, logement_agence la
+	WHERE la.idAgence=a.idAgence AND a.nom='laforet');
+-- or
+-- Version 2
+SELECT l.* 
+FROM logement l 
+INNER JOIN logement_agence la ON la.idLogement = l.idLogement
+INNER JOIN agence a ON a.idAgence=la.idAgence
+WHERE a.nom='laforet';
+
 /* Question 15 : Affichez le nombre de propriétaires dans la ville de Paris (Alias : Nombre) */
+SELECT COUNT(DISTINCT idPersonne) Nombre
+FROM logement_personne
+WHERE idLogement IN
+    (SELECT idLogement
+     FROM logement
+     WHERE ville = "Paris");
+
 /* Question 16 : Affichez les informations des trois premieres personnes souhaitant acheter un logement */
+SELECT * 
+FROM personne
+WHERE idPersonne IN (SELECT idPersonne
+     FROM demande
+     WHERE categorie = 'vente') 
+LIMIT 3;
+select * from logement;
 /* Question 17 : Affichez le prénom du vendeur pour le logement ayant la référence « 5770 » */
+SELECT p.prenom
+FROM personne p 
+INNER JOIN logement_personne lp ON lp.idPersonne = p.idPersonne
+INNER JOIN logement l ON l.idLogement=lp.idLogement
+WHERE l.idLogement=5770 AND l.categorie = 'vente';
+
 /* Question 18 : Affichez les prénoms des personnes souhaitant accéder à un logement sur la ville de Lyon */
+SELECT p.prenom
+FROM personne p 
+INNER JOIN demande d ON d.idPersonne = p.idPersonne
+WHERE d.ville='lyon';
+
 /* Question 19 : Affichez les prénoms des personnes souhaitant accéder à un logement en location sur la ville de Paris */
+SELECT p.prenom
+FROM personne p 
+INNER JOIN demande d ON d.idPersonne = p.idPersonne
+WHERE d.ville='paris' AND d.categorie = 'location';
+
 /* Question 20 : Affichez les prénoms des personnes souhaitant acheter un logement de la plus grande à la plus petite superficie */
-/* Question 21 : Quel sont les prix finaux proposés par les agences pour la maison à la vente ayant la référence « 5091 » ? (Alias : prix frais d'agence
-inclus) */
+SELECT p.prenom
+FROM personne p 
+INNER JOIN demande d ON d.idPersonne = p.idPersonne
+WHERE d.categorie = 'vente' ORDER BY superficie DESC;
+
+/* Question 21 : Quel sont les prix finaux proposés par les agences pour la maison à la vente ayant la référence « 5091 » ? (Alias : prix frais d'agence inclus) */
+SELECT (l.prix + la.frais) AS prixFinaux 
+FROM logement l
+INNER JOIN logement_agence la ON la.idLogement = l.idLogement
+WHERE l.idLogement = 5091;
+
 /* Question 23 : Si l’ensemble des logements étaient vendus ou loués demain, quel serait le bénéfice généré grâce aux frais d’agence et pour chaque agence
 (Alias : benefice, classement : par ordre croissant des gains) */
+
 /* Question 24 : Affichez les id des biens en location, les prix, suivis des frais d’agence (classement : dans l’ordre croissant des prix) */
 /* Question 25 : Quel est le prénom du propriétaire proposant le logement le moins cher à louer ? */
 /* Question 26 : Affichez le prénom et la ville où se trouve le logement de chaque propriétaire */
